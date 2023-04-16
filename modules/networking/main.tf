e433ad075d4b1b8427b37ce468b72e006a60401d
Nichole Dwight <nicholedwight@gmail.com>
2023-04-16 13:53:53 -0600

    fixed importing module

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -10,8 +10,8 @@ resource "aws_vpc" "wp_vpc" {
 
 /*=== Subnets ===*/
 resource "aws_subnet" "public_sub" {
-  vpc_id     = aws_vpc.wp_vpc.id
-  cidr_block = "10.0.1.0/16"
+  vpc_id                  = aws_vpc.wp_vpc.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
   tags = {
@@ -20,8 +20,8 @@ resource "aws_subnet" "public_sub" {
 }
 
 resource "aws_subnet" "private_sub" {
-  vpc_id     = aws_vpc.wp_vpc.id
-  cidr_block = "10.0.2.0/16"
+  vpc_id                  = aws_vpc.wp_vpc.id
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = false
 
   tags = {
