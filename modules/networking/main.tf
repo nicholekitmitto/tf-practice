25c13793b01e71c0171cc748e485752c15422c3b
Nichole Dwight <nicholedwight@gmail.com>
2023-04-16 16:57:31 -0600

    moved instance resources from instance module into networking - it caused a cycle error referencing each other

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,5 +1,4 @@
 /*=== VPC ===*/
-
 resource "aws_vpc" "wp_vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -29,5 +28,113 @@ resource "aws_subnet" "private_sub" {
   }
 }
 
-/*=== IGW ===*/
+/*=== ENI ===*/
+resource "aws_network_interface" "wp_eni" {
+  subnet_id   = aws_subnet.private_sub.id
+  private_ips = ["10.0.0.50"]
 
+  attachment {
+    instance     = aws_instance.wp_instance.id
+    device_index = 1
+  }
+
+  tags = {
+    Name = "private_network_interface"
+  }
+}
+
+output "wp_eni" {
+  value = aws_network_interface.wp_eni.id
+}
+/*=== NAT === */
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_network_interface.wp_eni.id
+  subnet_id     = aws_subnet.public_sub.id
+
+  tags = {
+    Name = "pub_nat"
+  }
+}
+
+/*=== IGW ===*/
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.wp_vpc.id
+
+  tags = {
+    Name = "wp_igw"
+  }
+}
+
+
+/*== Route Tables ===*/
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.wp_vpc.id
+
+  tags = {
+    Name = "public_rt"
+  }
+}
+
+resource "aws_route" "igw_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_sub.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.wp_vpc.id
+
+  tags = {
+    Name = "private_rt"
+  }
+}
+
+resource "aws_route" "nat_route" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private_sub.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+
+/*=== Instance ===*/
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "wp_instance" {
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = "t3.micro"
+
+  network_interface {
+    network_interface_id = aws_network_interface.wp_eni.id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = "wp_instance"
+  }
+}
+
+output "wp_instance" {
+  value = aws_instance.wp_instance.id
+}
