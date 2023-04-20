960e69acbadc007ec084505217132d9cd09420a6
Nichole Dwight <nicholedwight@gmail.com>
2023-04-20 17:34:20 -0600

    working on adding in lb

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -129,3 +129,61 @@ resource "aws_instance" "wp_instance" {
     Name = "wp_instance"
   }
 }
+
+/*=== Security Group ===*/
+resource "aws_security_group" "lb_sg" {
+  name        = "lb_sg"
+  description = "Allow public internet traffic"
+  vpc_id      = aws_vpc.wp_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "http"
+    cidr_blocks = [aws_vpc.wp_vpc.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lb_sg"
+  }
+}
+
+/*=== Load Balancer/TG ===*/
+resource "aws_s3_bucket" "lb_logs" {
+  bucket = "wp_lb_logs_bucket"
+}
+
+resource "aws_lb" "wp_lb" {
+  name               = "wp_lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = aws_subnet.public_sub
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.id
+    prefix  = "wp-lb"
+    enabled = true
+  }
+}
+
+resource "aws_lb_target_group" "wp_lb_tg" {
+  name     = "wp_lb_tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.wp_vpc.id
+
+}
+
+resource "aws_lb_target_group_attachment" "wp_lb_attach" {
+  target_group_arn = aws_lb_target_group.wp_lb_tg.arn
+  target_id        = aws_instance.wp_instance.id
+  port             = 80
+}
